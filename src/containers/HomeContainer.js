9d053c1e0dfaa9660bce56323837a4eb4a0445c6
Angie <amonroy@novasolutionsystems.com>
2022-06-21 19:10:33 -0500

    commit a borrar

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -10,7 +10,7 @@ function HomeContainer() {
   const navigate = useNavigate();
 
   const getOrder = () => {
-    const url = 'https://giftcardsapidev';
+    const url = 'https://giftcardsapidev.azurewebsites.net/api/orders';
     const body = {
       partnerId: 'bonnus-test',
       ipAddress: '192.168.0.1',
